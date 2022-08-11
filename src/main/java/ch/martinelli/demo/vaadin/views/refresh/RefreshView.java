36dd6b7b704c8e2df8d40f93dd3c65f4dbf35ecf
Simon Martinelli <simon@72.services>
2022-08-11 15:58:59 +0200

    Added Grid tests

--- src/main/java/ch/martinelli/demo/vaadin/views/refresh/RefreshView.java
+++ src/main/java/ch/martinelli/demo/vaadin/views/refresh/RefreshView.java
@@ -4,8 +4,11 @@ import ch.martinelli.demo.vaadin.views.MainLayout;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.data.provider.CallbackDataProvider;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -14,18 +17,35 @@ import java.util.List;
 @Route(value = "refresh", layout = MainLayout.class)
 public class RefreshView extends VerticalLayout {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(RefreshView.class);
+
     private final List<String> data = new ArrayList<>();
+    private final CallbackDataProvider<String, Void> dataProvider;
 
     public RefreshView() {
         Grid<String> grid = new Grid<>();
         grid.addColumn(s -> s);
-        grid.setItems(data);
 
-        Button button = new Button("Add data", event -> {
+        dataProvider = new CallbackDataProvider<>(
+                query -> {
+                    LOGGER.info("fetch");
+                    return data.stream().skip(query.getOffset()).limit(query.getLimit());
+                },
+                query -> {
+                    LOGGER.info("count");
+                    return data.size();
+                },
+                s -> s);
+
+        grid.setItems(dataProvider);
+
+        Button addData = new Button("Add data", event -> {
             data.add(System.currentTimeMillis() + " ms");
-            grid.getDataProvider().refreshAll();
+            dataProvider.refreshAll();
         });
 
-        add(grid, button);
+        Button refresh = new Button("Refresh", event -> dataProvider.refreshAll());
+
+        add(grid, addData, refresh);
     }
 }
--- src/main/java/ch/martinelli/demo/vaadin/views/split/Split2View.java
+++ src/main/java/ch/martinelli/demo/vaadin/views/split/Split2View.java
@@ -11,36 +11,34 @@ import com.vaadin.flow.router.Route;
 
 import java.util.List;
 
-@PageTitle("Split")
-@Route(value = "split", layout = MainLayout.class)
-public class SplitView extends SplitLayout {
+@PageTitle("Split2")
+@Route(value = "split2", layout = MainLayout.class)
+public class Split2View extends SplitLayout {
 
     private final Span right;
     private final Span newRight;
 
-    public SplitView() {
+    public Split2View() {
         Grid<String> grid = new Grid<>();
         addToPrimary(grid);
 
         right = new Span("Right");
-        addToSecondary(right);
         newRight = new Span("New Right");
+        newRight.setVisible(false);
+        addToSecondary(right, newRight);
 
         grid.addColumn(s -> s);
         grid.addComponentColumn(s -> new Button(s, event -> {
             Notification.show("Hello from " + s);
             if (s.equals("C")) {
-                if (getSecondaryComponent().equals(right)) {
-                    addToSecondary(newRight);
-                }
+                right.setVisible(false);
+                newRight.setVisible(true);
             } else {
-                if (getSecondaryComponent().equals(newRight)) {
-                    addToSecondary(right);
-                }
+                right.setVisible(true);
+                newRight.setVisible(false);
             }
         }));
 
         grid.setItems(List.of("A", "B", "C"));
-
     }
 }
